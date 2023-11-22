90043a60639be71eb826daa4867e6aa4e4204da5
aliceixe <alicetlhame@gmail.com>
2023-11-23 00:05:52 +0200

    save6

--- src/components/common/signUp.jsx
+++ src/components/common/signUp.jsx
@@ -1,13 +1,41 @@
 import PageHeader from "./PageHeader"
+import Input from "./input";
+import { useFormik } from "formik";
+
 
 const SignUp = ( ) => {
+    const from = useFormik({
+        initialValues: {
+            email: "",
+            name: "",
+            password: "",
+        },
+        onSubmit(values) {
+
+        },
+    });
+
+
+
+
+
+
     return (
+       <>
         <PageHeader  
         title="Sign Up"
         description="Open a new account for free <3"
         />
-    );
 
+
+        <form>
+            <Input name="email" label="email" required  />
+            
+            
+         
+        </form>
+        </>
+    );
 };
 
 
